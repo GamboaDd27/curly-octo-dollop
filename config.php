f45ae180a21de6c40f02f55bb4ab47786943178a
David Gamboa <davidgamboa2727@gmail.com>
2021-12-17 10:37:45 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+  $host = 'localhost';
+  $db = 'databook_db';
+  $user = 'postgres';
+  $password = '2016@Black@ragon';
---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+$pdo = require 'Connect.php';
+
+
+echo "
+\$pdo: ". $pdo. "
+";
+
+// // pass the connect instance to the pg_query() method 
+// $response = pg_query($conn, 'SELECT * FROM origen');
+
+// /// close the connection to free memory
+// pg_close($conn);
+
+// // end of the PHP script
+// echo $response;
+// echo 'xd';
